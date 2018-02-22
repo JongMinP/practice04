8c032012cf8dbb816272f0c03e3a9757119739c4
bit <bit@DESKTOP-A8LCUFN>
2018-02-22 19:08:10 +0900

    pratice04

--- src/prob03/Marine.java
+++ src/prob03/Marine.java
@@ -1,19 +1,20 @@
 package prob03;
 
-public class Marine {
+public class Marine extends Unit {
 	// 현재 위치
-	private int x;
-	private int y; 
-	
-	void stimPack() { 
-		/* 스팀팩을 사용한다.*/
-	}	
 
-	void move( int x, int y ) { 
-		/* 지정된 위치로 이동 */ 
+	void stimPack() {
+		/* 스팀팩을 사용한다. */
 	}
-	
-	void stop() { 
-		/* 현재 위치에 정지 */ 
-	}	
+
+	@Override
+	public void move(int x, int y) {
+
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
 }
